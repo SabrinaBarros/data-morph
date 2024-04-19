2f3465b88e9e7c21a94d20c3b3c006fe250c28f0
¨Sabrina <sabrina.barros2045@gmail.com>
2024-04-19 01:40:38 -0300

    test: international to american patterns

---
+++ tests/internationalToAmerican/internationalToAmericanInteger.test.js
@@ -0,0 +1,70 @@
+import patternFormater from '../../index.js'
+import {expect} from 'chai'
+
+describe('Should format float numbers with ##,#00 pattern', () => {
+
+  it('Should format 100', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '100');
+
+    expect(result).to.equal('100');
+
+  });
+
+  it('Should format 1.000', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '1.000');
+
+    expect(result).to.equal('1000');
+
+  });
+
+  it('Should format 10.000', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '10.000');
+
+    expect(result).to.equal('10000');
+
+  });
+
+  it('Should format 100.000', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '100.000');
+
+    expect(result).to.equal('100000');
+
+  });
+
+  it('Should format 1.000.000', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '1.000.000');
+
+    expect(result).to.equal('1000000');
+
+  });
+
+  it('Should format 1.000.000.000', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '1.000.000.000');
+
+    expect(result).to.equal('1000000000');
+
+  });
+
+  it('Should format 0', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '0');
+
+    expect(result).to.equal('0');
+
+  });
+
+  it('Should format 89.756.465', () => {
+
+    const result = patternFormater.internationalToAmerican('##,#00', '89.756.465');
+
+    expect(result).to.equal('89756465');
+
+  });
+
+});
---
+++ tests/internationalToAmerican/internationalToAmericanVariedPatterns.test.js
@@ -0,0 +1,63 @@
+import { it } from 'mocha';
+import patternFormater from '../../index.js'
+import {expect} from 'chai'
+
+describe('Should format float numbers with #,##0.00 pattern', () => {
+
+  it('Should format 200,678', () => {
+
+    const result = patternFormater.internationalToAmerican('#,##0.000', '200,678');
+
+    expect(result).to.equal('200.678');
+
+  });
+
+  it('Should format 1.000,000', () => {
+
+    const result = patternFormater.internationalToAmerican('#,##0.000', '1.000,000');
+
+    expect(result).to.equal('1000.000');
+
+  });
+
+  it('Should format 1.000,8975', () => {
+
+    const result = patternFormater.internationalToAmerican('#,##0.0000', '1.000,8975');
+
+    expect(result).to.equal('1000.8975');
+
+  });
+
+  it('Should format 1.000,50000', () => {
+
+    const result = patternFormater.internationalToAmerican('#,##0.00000', '1.000,50000');
+
+    expect(result).to.equal('1000.50000');
+
+  });
+
+  it('Should format 0,365760', () => {
+
+    const result = patternFormater.internationalToAmerican('#,##0.000000', '0,365760');
+
+    expect(result).to.equal('0.365760');
+
+  });
+
+  it('Should format 0', () => {
+
+    const result = patternFormater.internationalToAmerican('#,##0.00', '0');
+
+    expect(result).to.equal('0.00');
+
+  });
+
+  it('Should format 1.000.000.000,89', () => {
+
+    const result = patternFormater.internationalToAmerican('#,##0.00', '1.000.000.000,89');
+
+    expect(result).to.equal('1000000000.89');
+
+  });
+
+});
