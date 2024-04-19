495d564f70b80510f5d1a2759f9b4883289578db
¨Sabrina <sabrina.barros2045@gmail.com>
2024-04-19 01:36:56 -0300

    test: american to international patterns

---
+++ tests/americanToInternational/americanToInternationalFloat2Digits.test.js
@@ -0,0 +1,94 @@
+import patternFormater from '../../index.js'
+import {expect} from 'chai'
+
+describe('Should format float numbers with #,##0.00 pattern', () => {
+
+  it('Should format 100.00', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '100.00');
+
+    expect(result).to.equal('100,00');
+
+  });
+
+  it('Should format 1000.00', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '1000.00');
+
+    expect(result).to.equal('1.000,00');
+
+  });
+
+  it('Should format 10000.00', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '10000.00');
+
+    expect(result).to.equal('10.000,00');
+
+  });
+
+  it('Should format 100000.00', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '100000.00');
+
+    expect(result).to.equal('100.000,00');
+
+  });
+
+  it('Should format 1000000.00', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '1000000.00');
+
+    expect(result).to.equal('1.000.000,00');
+
+  });
+
+  it('Should format 1000.50', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '1000.50');
+
+    expect(result).to.equal('1.000,50');
+
+  });
+
+  it('Should format 0.36', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '0.36');
+
+    expect(result).to.equal('0,36');
+
+  });
+
+  it('Should format .3657', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '.3657');
+
+    expect(result).to.equal('0,36');
+
+  });
+
+  it('Should format .3', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '.3');
+
+    expect(result).to.equal('0,30');
+
+  });
+
+  it('Should format 3.', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '3.');
+
+    expect(result).to.equal('3,00');
+
+  });
+
+  it('Should format 03.', () => {
+
+    const result = patternFormater.americanToInternational('#,##0.00', '03.');
+
+    expect(result).to.equal('03,00');
+
+  });
+
+});
