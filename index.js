2ba3a84f3e11bef7ad764f59dddb09818e546b82
¨Sabrina <sabrina.barros2045@gmail.com>
2024-04-23 19:46:40 -0300

    feat: main file

---
+++ index.js
@@ -0,0 +1,11 @@
+import draftToInternational from './src/draftToInternational.js';
+import internationalToSimple from './src/internationalToSimple.js';
+import internationalToAmerican from './src/internationalToAmerican.js';
+import americanToInternational from './src/americanToInternational.js';
+
+export default {
+  draftToInternational,
+  internationalToSimple,
+  internationalToAmerican,
+  americanToInternational,
+};
